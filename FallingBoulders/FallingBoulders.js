b8adc46ad34d0c8843367b1b78cf18b99397daa4
WaffleCohn <wafflecohn@gmail.com>
2016-11-04 10:17:18 -0500

    Arctic Slide and Falling Boulders

---
+++ FallingBoulders/FallingBoulders.js
@@ -0,0 +1,6 @@
+function playButton() {
+	var difficulty = document.elFormicus.difficultySelect.value;//document.getElementsByName("difficultySelect");
+	//var strUser = difficulty.options[difficulty.selectedIndex].value;
+	alert(difficulty);
+	//window.location.href="FallingBouldersGame.html";
+}
