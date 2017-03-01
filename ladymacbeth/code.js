61ad8b0354fce2c833e21512047555a1d0a42c79
WaffleCohn <wafflecohn@gmail.com>
2017-02-28 23:20:45 -0600

    mostly finished lady macbeth page

---
+++ ladymacbeth/code.js
@@ -0,0 +1,35 @@
+var home = document.getElementById("home"),
+	video = document.getElementById("videoPage"),
+	story = document.getElementById("storyPage"),
+	storyContainer = document.getElementById("storyContainer");
+
+// display story
+for (var i=0; i < storyJSON.paragraphs.length; i++)
+{
+	var str = storyJSON.paragraphs[i];
+	if (~str.indexOf("CENTER:"))
+		storyContainer.innerHTML += "<p style='text-align: center;'>" + str.replace("CENTER:", "") + "</p>";
+	else if (~str.indexOf("BLOCKQUOTE:"))
+		storyContainer.innerHTML += "<p class='blockquote'>" + str.replace("BLOCKQUOTE:", "") + "</p>";
+	else
+		storyContainer.innerHTML += "<p>" + str + "</p>";
+}
+
+function displayVideo()
+{
+	home.style.display = "none";
+	video.style.display = "block";
+}
+
+function displayStory()
+{
+	home.style.display = "none";
+	story.style.display = "block";
+}
+
+function returnHome()
+{
+	home.style.display = "block";
+	video.style.display = "none";
+	story.style.display = "none";
+}
