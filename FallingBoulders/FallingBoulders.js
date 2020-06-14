edd60d825f1f5346efdb7b8682320c417237d780
WaffleCohn <wafflecohn@gmail.com>
2020-06-14 18:35:36 -0500

    started empty react app

--- FallingBoulders/FallingBoulders.js
+++
@@ -1,6 +0,0 @@
-function playButton() {
-	var difficulty = document.elFormicus.difficultySelect.value;//document.getElementsByName("difficultySelect");
-	//var strUser = difficulty.options[difficulty.selectedIndex].value;
-	alert(difficulty);
-	//window.location.href="FallingBouldersGame.html";
-}
